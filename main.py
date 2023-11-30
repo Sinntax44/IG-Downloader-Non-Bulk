a99ffcd6a33e1c7da815c96b6b05279293bd3e60
Sinntax44 <79892857+Sinntax44@users.noreply.github.com>
2023-11-30 23:02:37 +0800

    Update main.py
    modified to match instagram.py

--- main.py
+++ main.py
@@ -21,9 +21,8 @@ def saveJSON(timestamp: int, type: str, content: dict):
     path = os.path.join(dirpath, "json", str(timestamp) + "_" + type + ".json")
     dirpath = os.path.dirname(path)
     os.makedirs(dirpath, exist_ok=True)
-    f = open(path, "tx")
-    json.dump(content, f)
-    f.close()
+    with open(path, "tx") as f:
+        json.dump(content, f)
 
 def main():
     """ Main function
@@ -36,42 +35,24 @@ def main():
 
     try:
         logging.info("Opening config file.")
-        f = open(configfile, "tr")
+        with open(configfile, "tr") as f:
+            config = json.load(f)
     except FileNotFoundError:
         logging.info("File not found.")
         logging.info("Creating new config file.")
-        f = open(configfile, "tx+")
-
-    try:
-        logging.info("Loading config file.")
-        config = json.load(f)
-    except json.decoder.JSONDecodeError:
-        logging.info("Config file corrupted.")
-        logging.info("Creating new config file.")
-        f.close()
-        f = open(configfile, "tw+")
-        logging.info("Updating config.")
-        iguserid = input("Enter your IG user ID: ")
-        igsessionid = input("Enter your IG session ID: ")
-        igcsrftoken = input("Enter your IG CSRF token: ")
-        igmid = input("Enter your IG mid: ")
         config = {
-            "ds_user_id": iguserid,
-            "sessionid" : igsessionid,
-            "csrftoken" : igcsrftoken,
-            "mid" : igmid,
+            "ds_user_id": "",
+            "sessionid": "",
+            "csrftoken": "",
+            "mid": "",
         }
-        logging.info("Saving config.")
-        json.dump(config, f) # Save config
-
-    #logging.info("Config settings:")
-    #logging.info("%s", config) # Contains private data
-    f.close()
+        with open(configfile, "tx+") as f:
+            json.dump(config, f)
 
     # Insert error checking to see if config is valid and works
 
     logging.info("Initialize IG interface.")
-    ig = instagram.instagram(config)
+    ig = instagram.Instagram(config)
     logging.info("Get story tray.")
     traytime = int(time.time())
     storyresp = ig.getStories()
@@ -79,17 +60,19 @@ def main():
     saveJSON(traytime, "tray", storyjson)
 
     logging.info("Downloading story tray.")
-    ig.downloadTray(storyjson)
+    # Replace 'your_target_user_id' with the actual user ID you want to download
+    target_user_id = 'your_target_user_id'
+    ig.downloadTray(storyjson, target_user_id)
     users = ig.getUserIDs(storyjson)
     for user in users:
         reeltime = int(time.time())
         uresp = ig.getUserStories(user)
         ujson = uresp.json()
         saveJSON(reeltime, "reel_" + str(user), ujson)
-        ig.downloadReel(ujson)
+        ig.downloadReel(ujson, target_user_id)
 
     logging.info("Downloading post-live stories.")
-    ig.downloadStoryLive(storyjson)
+    ig.downloadStoryLive(storyjson, target_user_id)
 
     logging.info("Collecting list of JSON objects.")
     jsonlist = []
@@ -99,10 +82,9 @@ def main():
 
     logging.info("Creating tar.xz file of JSON objects.")
     path = os.path.join(os.getcwd(), "json", str(traytime) + ".tar.xz")
-    tar = tarfile.open(path, "x:xz")
-    for path in jsonlist:
-        tar.add(path)
-    tar.close()
+    with tarfile.open(path, "x:xz") as tar:
+        for path in jsonlist:
+            tar.add(path)
 
     logging.info("Removing old JSON objects.")
     for path in jsonlist:
